e9af05949b5a9cb58bea9f975a293784a561b8b3
indimind-web <indimindservices@gmail.com>
2024-09-02 20:31:49 +0530

    fixing error for meta data keywords

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,10 +10,7 @@ export const metadata: Metadata = {
     template: "%s | indimind",
   },
   description: "Indimind is an india based consultancy agency, which provides soltion for many fields such as mechanical, interior designing, marketing, web developement, graphic designing, 'digital marketing and much more......",
-  keywords: {
-    default: ['indimind', 'web development service', 'consultancy services', 'best consultancy', 'part manufacturing service', 'graphic design service', 'digital marketing service','indimind services','next.js website development','react website'],
-    template: [ '%s']
-  },
+  keywords: ['indimind', 'web development service', 'consultancy services', 'best consultancy', 'part manufacturing service', 'graphic design service', 'digital marketing service','indimind services','next.js website development','react website'],
   openGraph: {
     title: "indimind.in",
     description:
