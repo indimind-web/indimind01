6dc17b0c1d48d638aeb7d5dd1c2bac398cb46d38
indimind-web <indimindservices@gmail.com>
2024-09-09 10:59:06 +0530

    updated metadata in single service page

--- app/services/[slug]/page.tsx
+++ app/services/[slug]/page.tsx
@@ -2,17 +2,81 @@ import React from 'react'
 import {services} from "@/util/services"
 import Cad from '../components/Cad'
 import Npd from '../components/Npd'
-import Id from '../components/Id'
+import Id, { metadata as idMetadata} from '../components/Id'
 import Gd from '../components/Gd'
 import Ta from '../components/Ta'
 import Rd from '../components/Rd'
 import Pm from '../components/Pm'
 import Wd from '../components/Wd'
 import Dm from '../components/Dm'
+import { Metadata, ResolvingMetadata } from 'next'
+import { metadata as cadMetadata } from '../components/Cad'
 type Props = {
     params:{
         slug: string
     }
+}
+export async function generateMetadata(
+  { params }:Props): Promise<Metadata> {
+  // read route params
+  const slug = params.slug
+     if(slug == "cad"){
+    return cadMetadata
+  }
+  if(slug == "npd"){
+    return{
+      title:"indimind - New product development service"
+    }
+  }
+  if(slug == "id"){
+    return idMetadata
+  }
+  if(slug == "gd"){
+    return {
+      title:"indimind - graphic designing service"
+    }
+  }
+  if(slug == "ta"){
+    return {
+      title:"indimind - talent aquisition"
+    }
+  }
+  if(slug == "rd"){
+    return {
+      title:"indimind - research and development service"
+    }
+  }
+  if(slug == "pm"){
+    return {
+      title:"indimind - product manufacturing"
+    }
+  }
+  if(slug == "wd"){
+    return {
+      title:"indimind - web development service"
+    }
+  }
+  if(slug == "dm"){
+    return {
+      title:"indimind - digital marketing service"
+    }
+  }
+  return {
+    title: "indimind",
+    description: "Indimind is an india based consultancy agency, which provides soltion for many fields such as mechanical, interior designing, marketing, web developement, graphic designing, 'digital marketing and much more......",
+    keywords: [
+      'indimind',
+      'web development service',
+      'consultancy services',
+      'best consultancy',
+      'part manufacturing service',
+      'graphic design service',
+      'digital marketing service',
+      'indimind services',
+      'next.js website development',
+     'react website'
+    ]
+  }
 }
 
 const page = ({ params }: Props) => {
