c92fc4be61723dd624c11b665753084ebe68c411
dev patel <alltoexclusivegit@gmail.com>
2026-02-18 20:14:13 +0530

    Remove deprecated @next/font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,10 @@
 import "../global.css";
-import { Inter } from "@next/font/google";
-import LocalFont from "@next/font/local";
+import { Inter } from 'next/font/google'
+import LocalFont from "next/font/local";
 // import { Metadata } from "next";
 import { Analytics } from "./components/analytics";
 import Popup from "./components/popup"
-import {GoogleTagManager} from "@next/third-parties/google"
+import { GoogleTagManager } from "@next/third-parties/google"
 // export const metadata: Metadata = {
 //   title: {
 //     default: "indimind",
